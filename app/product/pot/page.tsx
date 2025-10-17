4a4a4a569c0f02d87f50a086c8f02c7f71f36f98
leenawatH <leenawat.hon@gmail.com>
2025-10-17 11:45:56 +0700

    add export const dynamic = force-dynamic;

--- app/product/pot/page.tsx
+++ app/product/pot/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { getAllSinglePotInCard } from '@/lib/service/potService';
 import PotFilterClient from '@/components/FilterClient/PotFilterClient';
 
