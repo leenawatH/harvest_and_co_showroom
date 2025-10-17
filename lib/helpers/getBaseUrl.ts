31899c03e1853dd7506ab68351e561134c1ff9ca
leenawatH <leenawat.hon@gmail.com>
2025-10-17 12:01:44 +0700

    fix base url

--- lib/helpers/getBaseUrl.ts
+++ lib/helpers/getBaseUrl.ts
@@ -1,2 +1,2 @@
 export const getBaseUrl = () =>
-  process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000' || 'https://harvest-and-co-showroom.vercel.app';
